297a689b252f224a1423bc65aeb2689c8b892ab3
Frederick Elledge <frederickjye@gmail.com>
2026-05-21 19:56:23 +0100

    more testing

---
+++ Calculator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- C# Sharp calculator ---");
+            Console.WriteLine("enter your name for the log");
+            Console.WriteLine("First name");
+            var fname = Console.ReadLine();
+            Console.WriteLine("last name");
+            var lname = Console.ReadLine();
+            Console.WriteLine(ProperCase(fname, lname));
+            
+            Console.WriteLine("Enter your first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your operator");
+            string op = Console.ReadLine();
+            Console.WriteLine("Enter your second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = 0;
+
+            //this is a switch expression
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("invalid operation");
+                    return;
+            }
+
+
+           
+            Console.WriteLine($"{num1}{op}{num2}={result}");
+            Console.ReadKey();
+
+            string ProperCase(string fname, string lname)
+            {
+                string fnameLower = fname.ToLower();
+                string lnameLower = lname.ToLower();
+
+                string newFname = char.ToUpper(fname[0]) + fnameLower.Substring(1);
+                string newLname = char.ToUpper(lname[0]) + lnameLower.Substring(1);
+
+                return newFname + " " + newLname;
+
+
+            }
+
+            //this is a switch statement 
+            char convertPointsToGrade(int points)
+            {
+                return points switch
+                {
+                    10 or 9 => 'A',
+                    8 or 7 or 6 => 'B',
+                    5 or 4 or 3 => 'C',
+                    2 or 1 => 'D',
+                    0 => 'E',
+                    _ => '!',
+                };
+            }
+        }
+    }
+}
+//tryparse()
+
+//try parse is the better version of this method
--- TodoList2026/Program.cs
+++ TodoList2026/Program.cs
@@ -1,72 +1,152 @@
 ﻿
-using System.ComponentModel.Design;
-using System.Net.Security;
+//using System.ComponentModel.Design;
+//using System.Net.Security;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+//Console.ReadKey();
+
+//bool IsOn = true;
+
+//Console.WriteLine("starting app");
+//Console.WriteLine("what would you like to do today");
+//Console.WriteLine("type S to see all TODOS");
+//Console.WriteLine("type A to add a TODO");
+//Console.WriteLine("type R to remove a TODO ");
+//Console.WriteLine("type E to exit");
+
+//string userinput = Console.ReadLine();
+//var userTodos = "";
+
+
+
+//Console.WriteLine(userinput);
+
+
+////Currently this while statement will indeffo print the selected input to the terminal 
+//while (IsOn)
+//{
+//    if (userinput == "S")
+//    {
+//        PrintSelectedOption("See all Todos", userinput);
+//    }
+//    else if (userinput == "A")
+//    {
+//        PrintSelectedOption("Add TODO", userinput);
+//    }
+//    else if (userinput == "R")
+//    {
+//        PrintSelectedOption("Remove TODO", userinput);
+//    }
+//    else if (userinput == "E")
+//    {
+//        PrintSelectedOption("Exiting program", userinput);
+//        Console.WriteLine("Exiting the program...");
+//        IsOn = false;
+
+//    }
+//    else
+//    {
+//        Console.WriteLine("Unrecognized input. Exiting the program...");
+//        IsOn = false;
+//    }
+//}
+
+//Console.WriteLine("The app is closed. Press any key to exit.");
+//Console.ReadKey();
+
+//void PrintSelectedOption(string selectedOption, string userinput)
+//{
+//    Console.WriteLine($"{userinput} selected option: " + selectedOption);
+//}
+
+
+
+//dotnet run "run the program make sure pointed at folder that contains .csproj extention"
+//dotnet build "build the application without running it"
+//dotnet new class -n MyNewClass "create a new file for this class"
+//*/
+
+
+
+
+//Count to one million timer
+using System.Diagnostics;
+Console.WriteLine("press any key to start");
 Console.ReadKey();
 
-bool IsOn = true;
-
-Console.WriteLine("starting app");
-Console.WriteLine("what would you like to do today");
-Console.WriteLine("type S to see all TODOS");
-Console.WriteLine("type A to add a TODO");
-Console.WriteLine("type R to remove a TODO ");
-Console.WriteLine("type E to exit");
-
-string userinput = Console.ReadLine();
-var userTodos = "";
-
-
-
-Console.WriteLine(userinput);
-
-
-//Currently this while statement will indeffo print the selected input to the terminal 
-while (IsOn)
+var number = 0;
+var countTil = 1000000;
+Stopwatch watch = Stopwatch.StartNew();
+while (number < countTil)
 {
-    if (userinput == "S")
-    {
-        PrintSelectedOption("See all Todos", userinput);
-    }
-    else if (userinput == "A")
-    {
-        PrintSelectedOption("Add TODO", userinput);
-    }
-    else if (userinput == "R")
-    {
-        PrintSelectedOption("Remove TODO", userinput);
-    }
-    else if (userinput == "E")
-    {
-        PrintSelectedOption("Exiting program", userinput);
-        Console.WriteLine("Exiting the program...");
-        IsOn = false;
-
-    }
-    else
-    {
-        Console.WriteLine("Unrecognized input. Exiting the program...");
-        IsOn = false;
-    }
+    number++;
+    //Console.WriteLine(number); //remove this time as it is slows down the whole operation
 }
+watch.Stop();
+Console.WriteLine("exit loop");
+Console.WriteLine($"Time elapsed: {watch.Elapsed.TotalMilliseconds:F4} milliseconds");
+Console.WriteLine($"Time elapsed: {watch.Elapsed.TotalSeconds:F2} seconds");
+Console.WriteLine($"Time elapsed: {watch.ElapsedMilliseconds} milliseconds");
 
-Console.WriteLine("The app is closed. Press any key to exit.");
+Console.WriteLine($"counts per millisecond: {countTil / watch.Elapsed.TotalMilliseconds}");
+
 Console.ReadKey();
 
-void PrintSelectedOption(string selectedOption, string userinput)
-{
-    Console.WriteLine($"{userinput} selected option: " + selectedOption);
-}
+
+////Exerise 
+//using System.Reflection.Metadata.Ecma335;
+//using System.Threading.Channels;
+
+/////*terminal commands#
+//int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+//{
+
+//    int currentNumber = firstNumber;
+//    int sum = 0; 
+//    while (currentNumber <= lastNumber)
+//    {
+//        sum += currentNumber;
+//        currentNumber++;
+//    }
+//    return sum;
+//}
+
+//Console.WriteLine("enter the first number");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enter the second number");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(CalculateSumOfNumbersBetween(num1, num2));
+//Console.ReadKey();
+
+// the difference between ++i and i++ 
+//int i = 5;
+//Console.WriteLine(i++); // prints 5, then i becomes 6
+//Console.WriteLine(i);   // prints 6
+
+//int j = 5;
+//Console.WriteLine(++j); // j becomes 6, then prints 6
+//Console.WriteLine(j);   // prints 6
+
+//Console.ReadKey();
 
 
-int Add (int a, int b)
-{
-    return a + b;
+//Excerse
 
+//static string RepeatCharacter(char character, int targetLength)
+//{
+//    string result = "";
+//    do
+//    {
+//        result += character;
+//    }
+//    while (result.Length < targetLength);
+//    return result;
+//}
 
-/*terminal commands 
-dotnet run "run the program make sure pointed at folder that contains .csproj extention"
-dotnet build "build the application without running it" 
-dotnet new class -n MyNewClass "create a new file for this class" 
-*/
+//Console.WriteLine("Enter the char");
+//char character = Convert.ToChar(Console.ReadLine());
+//Console.WriteLine("enter the targe length");
+//int targetLength = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine(RepeatCharacter(character, targetLength));
+//Console.ReadKey(); 
